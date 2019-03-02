053295281c35bdeca0166687ebb25a609e644bef
Nishit Asnani <nishitasnani@DN0a22b45d.SUNet>
2019-03-02 14:38:15 -0800

    Init sentence vectors file

---
+++ utils/sent_vecs.py
@@ -0,0 +1,15 @@
+import numpy as np
+import gensim as gs
+
+
+def gensim_sent2vec(sentences):
+    pass
+
+
+def gensim_word2vec_mean(sentences):
+    pass
+
+
+def bert_sent_vecs(sentences):
+    pass
+

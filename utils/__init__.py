488c601692c637f67a5dd81e67c410c7fa63f6a5
Ubuntu <skeleton@rbadlani-nasnani-vm.dyczmd0xdsaejiutj0pkrnzd2a.bx.internal.cloudapp.net>
2019-03-06 00:14:35 +0000

    Sentence pair level distinguishing using cosine and/or euclidean

---
+++ utils/__init__.py
@@ -0,0 +1,3 @@
+from .get_similarity_metrics import *
+from .sentence_similarity import SentenceSimilarity
+from .dataset import SkeletonDataset
--- utils/get_similarity_metrics.py
+++ utils/get_similarity_metrics.py
@@ -1,5 +1,7 @@
 import numpy as np
-from sentence_similarity import SentenceSimilarity
+import scipy
+
+from .sentence_similarity import SentenceSimilarity
 
 
 def load_file(filename):
@@ -22,18 +24,52 @@ def separate_into_sents_and_skeletons(paragraphs):
     return sentences, skeletons
 
 
-def get_cosine_similarities(sentences):
+def get_cosine_similarities(sentences, filename=''):
     """Return a list of cosine similarities between pairs of consecutive sentences
     or skeletons
     """
     sim_object = SentenceSimilarity(sentences)
     sim_object.get_bert_sent_vecs()
+    if filename != '':
+        np.save(filename, sim_object.bert_sent_vecs)
     similarities = []
     for idx in range(len(sentences)-1):
         similarities.append(sim_object.get_bert_cosine_similarity(idx, idx+1))
     return similarities
 
 
+def get_cosine_sim_from_embs(embeddings):
+    similarities = []
+    nan_sims = 0
+    # print(embeddings[0].shape)
+    for idx in range(len(embeddings)-1):
+        cosine_distance = scipy.spatial.distance.cosine(embeddings[idx], embeddings[idx+1])
+        cosine_similarity = 1.0 - cosine_distance
+        if np.sum(np.isfinite(embeddings[idx])) < len(embeddings[idx]):
+            print(embeddings[idx])
+            break
+        # print(type(embeddings[idx]), type(embeddings[idx+1]))
+        if np.sum(np.isfinite(embeddings[idx+1])) < len(embeddings[idx+1]):
+            print(embeddings[idx+1])
+            break
+        
+        # euclidean_distance = scipy.spatial.distance.euclidean(embeddings[idx], embeddings[idx+1])
+        similarities.append(cosine_similarity)
+        if similarities[-1] != similarities[-1]:
+            # print(cosine_distance)
+            nan_sims += 1
+    print(nan_sims)
+    return similarities
+    
+
+def get_aggregate_similarity(similarities):
+    """
+    Return an aggregate of similarities of sentences for a paragraph  level score
+    Currently uses mean to aggregate. 
+    """
+    return np.mean(similarities)
+    
+
 if __name__ == '__main__':
-    filename = 'train.txt'
+    pass
     
